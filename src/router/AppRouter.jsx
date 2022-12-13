c46b2c427f19a1a8795cf799b763eeb0a15f8df6
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-12 22:22:21 -0600

    First commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Routes } from 'react-router-dom'
+
+export const AppRouter = () => {
+  return (
+    <Routes>
+      {/* Log in y Registro */}
+      {/* Journal app */}
+    </Routes>
+  )
+}
