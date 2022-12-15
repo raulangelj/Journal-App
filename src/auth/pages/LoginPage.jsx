86c12b2e6146d597d8093bb0f7a470a075d33502
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-14 20:51:01 -0600

    chore: add AuthLayout for login

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import React from "react";
+import { AuthLayout } from "../layout";
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
-  )
-}
+    <AuthLayout title="Login">
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Email"
+              type="email"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Password"
+              type="password"
+              placeholder="correo@google.com"
+              fullWidth
+            />
+          </Grid>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant="contained" fullWidth>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container direction="row" justifyContent="end">
+            <Link component={RouterLink} color="inherit" to="/auth/register">
+              Sign in
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
+    </AuthLayout>
+  );
+};
