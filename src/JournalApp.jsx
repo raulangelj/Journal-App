ef01eecc113c37fa5d295e5e5385946acee25830
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-14 19:41:35 -0600

    chore: add material UI

--- src/JournalApp.jsx
+++ src/JournalApp.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { AppTheme } from '../theme'
 import { AppRouter } from './router/AppRouter'
 
 export const JournalApp = () => {
   return (
-    <>
+    <AppTheme>
       <AppRouter />
-    </>
+    </AppTheme>
   )
 }
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,7 +1,11 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 export const JournalPage = () => {
   return (
-    <div>JournalPage</div>
+    <>
+      {/* <Typography component='h1'>JournalPage</Typography> */}
+      <Typography variant='h1'>JournalPage</Typography>
+    </>
   )
 }
