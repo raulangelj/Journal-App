cdbcffd76629253265547be699ac1ae4e52045af
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-22 16:08:01 -0600

    FIX imports useCheckAuth

--- src/shared/hooks/useCheckAuth/useCheckAuth.js
+++ src/shared/hooks/useCheckAuth/useCheckAuth.js
@@ -2,7 +2,7 @@ import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { FirebaseAuth } from "../../../firebase/config";
-import { login } from "../../../store/auth";
+import { login, logout } from "../../../store/auth";
 
 export const useCheckAuth = () => {
   const dispatch = useDispatch();
