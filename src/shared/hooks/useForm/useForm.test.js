a83a8328210768aef99dde636074bb1a855b3fc8
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-23 23:08:53 -0600

    fix: useForm test

--- src/shared/hooks/useForm/useForm.test.js
+++ src/shared/hooks/useForm/useForm.test.js
@@ -1,54 +1,57 @@
-import { renderHook } from "@testing-library/react"
+import { renderHook } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import { useForm } from "./useForm";
 
-describe('Testing useForm', () => {
+describe("Testing useForm", () => {
   const initialForm = {
-    name: 'Raul',
-    email: 'raulangelj@gmail.com'
-  }
+    name: "Raul",
+    email: "raulangelj@gmail.com",
+  };
 
-  test('Return default values', () => {
+  test("Return default values", () => {
     const { result } = renderHook(() => useForm(initialForm));
     expect(result.current).toEqual({
       name: initialForm.name,
       email: initialForm.email,
       formState: initialForm,
+      isFormValid: true,
       onInputChange: expect.any(Function),
-      onResetForm: expect.any(Function)
+      onResetForm: expect.any(Function),
     });
-  })
+  });
 
-  test('Change the name of the form', () => {
-    const newValue = 'John Doe';
+  test("Change the name of the form", () => {
+    const newValue = "John Doe";
     const { result } = renderHook(() => useForm(initialForm));
     const { onInputChange } = result.current;
     act(() => {
-      onInputChange({ target: { name: 'name', value: newValue }});
-    })
+      onInputChange({ target: { name: "name", value: newValue } });
+    });
     const { name, formState } = result.current;
     expect(name).toBe(newValue);
     expect(formState.name).toBe(newValue);
-  })
+  });
 
-  test('Rest the form', () => {
-    const newValue = 'John Doe';
+  test("Rest the form", () => {
+    const newValue = "John Doe";
     const { result } = renderHook(() => useForm(initialForm));
     const { onInputChange, onResetForm } = result.current;
     act(() => {
-      onInputChange({ target: { name: 'name', value: newValue }});
+      onInputChange({ target: { name: "name", value: newValue } });
       onResetForm();
-    })
+    });
     const { name, formState } = result.current;
     expect(name).toBe(initialForm.name);
     expect(formState.name).toBe(initialForm.name);
-  })
+  });
 
-  test('Validate the form', () => {
-    const { result } = renderHook(() => useForm(initialForm, {
-      name: [(value) => value.length > 0, 'The name is required'],
-      email: [(value) => value.includes('@'), 'The email should contain @']
-    }));
+  test("Validate the form", () => {
+    const { result } = renderHook(() =>
+      useForm(initialForm, {
+        name: [(value) => value.length > 0, "The name is required"],
+        email: [(value) => value.includes("@"), "The email should contain @"],
+      })
+    );
     const { formState, errors, isFormValid } = result.current;
-  })
-})
+  });
+});
