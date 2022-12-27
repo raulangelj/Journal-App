31e7f3bd9f6f929940eabc0fac4f4f9dbf1762b6
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-12-27 00:06:36 -0600

    FIX for process.env vars

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,10 +1,8 @@
 export const getEnvironments = () => {
   if (typeof process !== "undefined") {
-    return { ...process.env}
+    return { ...process.env };
   } else {
-    try {
-      import.meta.env
-      return { ...import.meta.env}
-    } catch (error) {}
+    
+    return { ...import.meta.env };
   }
-}
+};
